f127dc09183037f44f6d59d0e00e2165b4552225
andy.ho <andy.ho@onelab.tw>
2023-12-05 17:52:25 +0800

    feat : add holiday stuff.

---
+++ OneBackComboTrainingWeb/Holiday.cs
@@ -0,0 +1,20 @@
+﻿namespace OneBackComboTrainingWeb;
+
+public class Holiday
+{
+    public string GetTheme()
+    {
+        var dateTime = GetToday();
+        if (dateTime.Month == 12 && dateTime.Day == 25)
+        {
+            return "Merry Xmas";
+        }
+
+        return "Today is not Xmas";
+    }
+
+    public virtual DateTime GetToday()
+    {
+        return DateTime.Now;
+    }
+}
---
+++ OneBackTests/HolidayTests.cs
@@ -0,0 +1,49 @@
+﻿using OneBackComboTrainingWeb;
+
+namespace OneBackTests;
+
+public class HolidayTests
+{
+    private HolidayForTest _holiday;
+    
+    
+
+    private void ThemeShouldBe(string expected)
+    {
+        Assert.That(expected, Is.EqualTo(_holiday.GetTheme()));                                                                         
+    }
+
+    [Test]
+    public void IsTodayXmas()
+    {
+        GivenToday(12, 25);
+        ThemeShouldBe("Merry Xmas");
+    }
+
+    [Test]
+    public void IsTodayNotXmas()
+    {
+        GivenToday(11, 25);
+        ThemeShouldBe("Today is not Xmas");
+    }
+
+    private void GivenToday(int month, int day)
+    {
+        _holiday.Today = new DateTime(2023, month, day);
+    }
+
+    public class HolidayForTest : Holiday
+    {
+        private DateTime _today;
+
+        public DateTime Today
+        {
+            set => _today = value;
+        }
+
+        public override DateTime GetToday()
+        {
+            return _today;
+        }
+    }
+}
